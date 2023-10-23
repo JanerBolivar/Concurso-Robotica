f72514a0680b89e5e25f53bedd0f805938920998
JanerBolivar <101657362+JanerBolivar@users.noreply.github.com>
2023-10-23 17:18:21 -0500

    Login Hecho
    Se logean con el super usuario

--- login/views.py
+++ login/views.py
@@ -1,13 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import View
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+from .forms import CustomUserCreationForm
 
-
-class LoginListView(View):
-    def get(self, request, *args, **kwargs):
-        context = {
-            
-        }
-        return render(request, 'Login.html', context)
     
 class LoginHomeView(View):
     def get(self, request, *args, **kwargs):
@@ -15,3 +11,13 @@ class LoginHomeView(View):
             
         }
         return render(request, 'Home.html', context)
+
+def exit(request):
+    logout(request)
+    return redirect('home')
+
+def registro(self, request, *args, **kwargs):
+    context = {
+        'form': CustomUserCreationForm
+    }
+    return render(request, 'registration/registro.html', context)
