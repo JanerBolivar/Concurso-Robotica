dbabf2eea17360ac21a25118033de86f46e4b8c1
JanerBolivar <101657362+JanerBolivar@users.noreply.github.com>
2023-11-28 17:50:03 -0500

    Acerca_de
    El acerca del y urls corregidas

--- competencia/urls.py
+++ competencia/urls.py
@@ -1,13 +1,16 @@
 from django.urls import path
 from .views import CrearCompetenciaView, PruebaCompetenciaView, crearCategoriaView, crear_RA_CategoriaView, crearReglaView, crearAreaEvaluacionView, agregar_CategoriaView, Mostrar_InformacionView
-from .views import ModificarCompetenciaView, AsignarJurdoView, InscripcionCompetenciaView
+from .views import ModificarCompetenciaView, AsignarJurdoView, InscripcionCompetenciaView, Inscripcion_ExitosaView, Mostrar_InformacionCompetenciaView, GestionarCompetenciasView
 
 app_name="competencia"
 
 urlpatterns = [
     path('crear/', CrearCompetenciaView.as_view(), name="crear"),
     path('prueba/', PruebaCompetenciaView.as_view(), name="prueba"),
+    path('mostrar-competencias/', Mostrar_InformacionCompetenciaView.as_view(), name="mostrar_competencias"),
+    path('gestionar-competencias/', GestionarCompetenciasView.as_view(), name="gestionar_competencias"),
     path('<int:competencia_id>/crear-categoria/', crearCategoriaView.as_view(), name='crear_categoria'),
+    path('<int:competencia_id>/inscripcion-exitosa/', Inscripcion_ExitosaView.as_view(), name='incripcion_exitosa'),
     path('<int:competencia_id>/inscripcion/', InscripcionCompetenciaView.as_view(), name='inscripcion_competencia'),
     path('<int:competencia_id>/categoria/<int:categoria_id>/agregar-reglas-areas_evaluacion/', crear_RA_CategoriaView.as_view(), name='crear_RA_categoria'),
     path('<int:competencia_id>/agregar-categorias/', agregar_CategoriaView.as_view(), name='agregar_categorias'),
--- core/urls.py
+++ core/urls.py
@@ -2,11 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 
-from core.views import HomeView
+from core.views import HomeView, AcercaDeView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', HomeView.as_view(), name="home"),
+    path('acerca-de', AcercaDeView.as_view(), name="acerca_de"),
     path('accounts/', include('django.contrib.auth.urls')),
     path('login/', include('login.urls', namespace='login')),
     path('competencia/', include('competencia.urls', namespace='competencia')),
